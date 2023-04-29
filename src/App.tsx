6741f01d79404acad54d5a04eb46a95f45f8ca8b
rafaxavier <rxn.90s@gmail.com>
2023-04-29 20:50:36 -0300

    feat: create chart with canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DonutChart from './components/DonutChart';
+
+const chartData = [
+  { label:"Faltas insjustificadas", value: 300, color: 'purple' },
+  { label:"Horário Britânico", value: 500, color: 'blue' },
+  { label:"Intrajornada", value: 200, color: 'red' },
+  { label:"Colaboradores com faltas", value: 150, color: 'orange' },
+  { label:"Interjornada", value: 252, color: 'green' },
+  { label:"Intrajornada", value: 200, color: 'red' },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DonutChart data={chartData}  />
       </header>
     </div>
   );
---
+++ src/components/DonutChart/index.tsx
@@ -0,0 +1,60 @@
+import { Box } from '@mui/material';
+import React, { useEffect, useRef } from 'react';
+
+interface DonutChartProps {
+  data: { label: string, value: number, color: string }[];
+}
+
+const DonutChart: React.FC<DonutChartProps> = ({ data }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+      let totalValue = 0;
+      data.forEach(value => totalValue += value.value);
+
+      let startAngle = 0;
+      data.forEach(({ label, value, color }, index) => {
+        const sliceAngle = 2 * Math.PI * value / totalValue;
+        const endAngle = startAngle + sliceAngle;
+      
+        // Desenha o segmento do gráfico
+        ctx.beginPath();
+        ctx.arc(canvas.width / 2, canvas.height / 2, canvas.height / 4, startAngle, endAngle);
+        ctx.lineTo(canvas.width / 2, canvas.height / 2);
+        ctx.fillStyle = color;
+        ctx.fill();
+      
+        // Adiciona o valor ao redor do gráfico
+        const midAngle = startAngle + sliceAngle / 2;
+        const textRadius = canvas.height / 2.6;
+        const x = canvas.width / 2 + Math.cos(midAngle) * textRadius;
+        const y = canvas.height / 2 + Math.sin(midAngle) * textRadius;
+        ctx.fillStyle = "grey";
+        ctx.font = "600 14px Arial";
+        ctx.fillText(label, x - ctx.measureText(label).width / 2, y - 15);
+        ctx.fillText(value.toString(), x - ctx.measureText(value.toString()).width / 2, y, 20);
+      
+        startAngle = endAngle;
+      });
+
+      // Desenha um novo círculo interno com a cor de fundo
+      const innerRadius = canvas.height / 8;
+      ctx.beginPath();
+      ctx.arc(canvas.width / 2, canvas.height / 2, innerRadius, 0, 2 * Math.PI);
+      ctx.fillStyle = "#fff"; // Cor de fundo da rosca
+      ctx.fill();
+    }
+  }, [data]);
+
+  return (
+    <Box p={5} m={5} sx={{width:'100%', border:'1px solid red'}}>
+      <canvas ref={canvasRef} width={350} height={350} />
+    </Box>
+  );
+};
+
+export default DonutChart;
