4913fb091090e0e94ada4caf383c307ca6cb576d
rafaxavier <rxn.90s@gmail.com>
2023-04-30 20:49:03 -0300

    feat: create DonutChartGoogle Component

---
+++ src/components/DonutChartGoogle/index.tsx
@@ -0,0 +1,44 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { Chart } from "react-google-charts";
+
+interface DonutChartGoogleProps {
+  dados?: any;
+}
+
+const DonutChartGoogle: React.FC<DonutChartGoogleProps> = ({ dados }) => {
+  let transformedData: {} | undefined = [];
+  if (dados) {
+    transformedData = [["Label", "Value"], ...dados];
+  }
+
+  const options = {
+    title: "My Daily Activities",
+    pieHole: 0.5,
+    is3D: false,
+    pieSliceText: "none",
+    pieSliceBorderColor: "transparent",
+    legend: {
+      position: "labeled",
+      textStyle: {
+        fontSize: 14,
+        color: "grey",
+      },
+    },
+  };
+
+  return (
+    <Box sx={{border:'1px solid black', height:'350px', }}>
+      <Chart
+      chartType="PieChart"
+      width="100%"
+      height="300px"
+      data={transformedData}
+      options={options}
+    />
+    </Box>
+    
+  );
+};
+
+export default DonutChartGoogle;
