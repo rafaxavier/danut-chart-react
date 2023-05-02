aa9e3e408743605b34feee4d6c105b13b1bf7a1b
rafa xavier <rxn.90s@gmail.com>
2023-05-02 19:26:02 -0300

    feat: create DonutApexChart component

---
+++ src/components/DonutApexChart/index.tsx
@@ -0,0 +1,64 @@
+import React, { useRef, useEffect } from "react";
+import ApexCharts from "apexcharts";
+import { ApexOptions } from "apexcharts";
+import { Box } from "@mui/material";
+
+interface DonutApexChartProps {
+  data: { labels: string[]; series: number[] };
+}
+
+const DonutApexChart: React.FC<DonutApexChartProps> = ({ data }) => {
+  const chartRef = useRef<HTMLDivElement>(null);
+
+  const chartOptions: ApexOptions = {
+    plotOptions: {
+      pie: {
+        expandOnClick: false,
+        donut: {
+          size: "50%",
+          labels: {
+            show: true, //exibe item no centro
+          }
+        },
+      }
+    },
+    chart: {
+      type: "donut",
+      width:'350px',
+    },
+    stroke:{
+      width:0, // retirando as bordas das fatias
+    },
+    series: data.series,
+    labels: data.labels,
+  };
+  
+  let chart: ApexCharts | undefined;
+
+  useEffect(() => {
+    if (chartRef.current) {
+      chart = new ApexCharts(chartRef.current, chartOptions);
+      chart.render();
+    }
+    return () => {
+      if (chart) {
+        chart.destroy();
+      }
+    };
+  }, [data]);
+
+  useEffect(() => {
+    if (chart && data) {
+      chart.updateOptions({
+        series: data.series,
+        labels: data.labels,
+      });
+    }
+  }, [data]);
+
+  return (
+    <Box m={2} p={2} sx={{height:'350px', border: '1px solid black' }} ref={chartRef} />
+  );
+};
+
+export default DonutApexChart;
