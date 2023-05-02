95a4467ddc3c7c9702e9c5e49d2e49c7034d1d45
rafa xavier <rxn.90s@gmail.com>
2023-05-02 19:26:39 -0300

    feat: call DonutApexChart component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import './App.css';
 import DonutChart from './components/DonutChart';
 import DonutChartGoogle from './components/DonutChartGoogle';
 import DonutLibChart from './components/DonutLibChart';
+import DonutApexChart from './components/DonutApexChart';
 
 const chartData = [
   { label:"Faltas insjustificadas", value: 300, color: '#0099c6' },
   { label:"Horário Britânico", value: 500, color: '#3366cc' },
   { label:"Intrajornada", value: 200, color: '#dc3912' },
-  { label:"Colaboradores com faltas", value: 150, color: 'orange' },
+  { label:"Colaboradores com faltas", value: 1, color: 'orange' },
   { label:"Interjornada", value: 252, color: 'green' },
   { label:"Intrajornada", value: 200, color: 'purple' },
 ];
@@ -17,24 +18,36 @@ const chartData = [
 const chartData2 = [
   ["Faltas insjustificadas", 300 ],
   ["Horário Britânico", 500],
-  ["Intrajornada", 200 ],
+  ["Intrajornada", 1 ],
   ["Colaboradores com faltas", 150 ],
   ["Interjornada", 252 ],
   ["Intrajornada", 200 ],
-]
-;
+];
+
+const data = {
+  labels: ["Apple", "Banana", "Orange"],
+  series: [44, 55, 13],
+};
 
 function App() {
   return (
-    <Box sx={{display:'flex'}}>
-      <Box sx={{width:'33%'}}>
-        <DonutChart data={chartData} />
+    <Box>
+      <Box sx={{display:'flex'}}>
+        <Box sx={{width:'50%'}}>
+          <DonutChart data={chartData} />
+        </Box>
+        <Box sx={{width:'50%'}}>
+          <DonutChartGoogle dados={chartData2} />
+        </Box>
       </Box>
-      <Box sx={{width:'34%'}}>
-        <DonutChartGoogle dados={chartData2} />
-      </Box>
-      <Box sx={{width:'33%'}}>
-        <DonutLibChart dados={chartData} />
+
+      <Box sx={{display:'flex'}}>
+        <Box sx={{width:'50%'}}>
+          <DonutApexChart data={data} />
+        </Box>
+        <Box sx={{width:'50%'}}>
+          <DonutLibChart dados={chartData} />
+        </Box>
       </Box>
     </Box>
   );
